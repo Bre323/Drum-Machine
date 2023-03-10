9feb953919340139d4cd642417d0d216a12b4d90
Breno Rocha <brenopenarocha2@gmail.com>
2023-03-10 15:13:15 -0300

    New directories and Keyboard Structure

---
+++ src/components/Keyboard.js
@@ -0,0 +1,40 @@
+import React from "react"
+
+
+const KeyboardKey = ({ play, sound }) => {
+
+    const handleKeyDown = (event) => {
+        if(event.keyCode === sound.keyCode) {
+            play(sound.keyTrigger, sound.id)
+        }
+    }
+
+    React.useEffect(() => {
+        document.addEventListener("keydown", handleKeyDown)
+    })
+
+    return (
+        <button className="drum-pad" id={sound.id} onClick={() => play(sound.keyTrigger, sound.id)}>
+
+            <audio className="clip" id={sound.keyTrigger} src={sound.url} />
+            {sound.keyTrigger}
+
+        </button>
+    )
+}
+
+
+const Keyboard = ({ play, sounds }) => {
+    return (
+        <div className="keyboard">
+            {
+                sounds.map((sound) => {
+                    return <KeyboardKey play={play} sound={sound} key={sound.id} />
+                })
+            }
+        </div>
+    )
+}
+
+
+export default Keyboard
