0ecaa91cff7598c74827d098e3d7529ae37ccaea
Breno Rocha <brenopenarocha2@gmail.com>
2023-03-11 10:29:47 -0300

    Adding Elements to DrumControl

--- src/components/DrumControl.js
+++ src/components/DrumControl.js
@@ -3,7 +3,13 @@ import '../styles/DrumControl.css'
 
 const DrumControl = () => {
     return (
-        <div className="control"></div>
+        <div className="control">
+            <button className="power-button">Turn OFF</button>
+            <p>Volume: 100</p>
+            <input type="range" min="0" max="1" step="0.01" />
+            <h2 id="display">display</h2>
+            <button className="change-button">Change Sounds Group</button>
+        </div>
     )
 }
 
